8cef92caff0345eddc7b932ea0addf0c67ebf3dd
Felix Herrmann <felix.herrmann@icloud.com>
2024-10-11 01:31:14 +0200

    Initial Commit

---
+++ SwiftPackageListCocoapodsExample/SwiftPackageListCocoapodsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPackageListCocoapodsExampleApp.swift
+//  SwiftPackageListCocoapodsExample
+//
+//  Created by Felix Herrmann on 11.10.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPackageListCocoapodsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
